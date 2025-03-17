ac00a0e29c4247cb87a7fa4c332a77c8316f03d6
Sean Morrissey <hey@bhmt.dev>
2025-03-17 12:53:06 -0400

    Update base URL handling

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,5 +1,9 @@
+// assign default values if env variables are empty
 const protocol = import.meta.env.VITE_SERVER_PROTOCOL || 'http'
 const domain = import.meta.env.VITE_SERVER_URL || 'localhost'
 const port = import.meta.env.VITE_SERVER_PORT || '3030'
 
-export const baseURL = `${protocol}://${domain}:${port}`
+const host = `${protocol}://${domain}`
+
+// include port only if it's a number
+export const baseURL = Number(port) ? `${host}:${port}` : host
